fe6bf8705ad9bf74ef317b128f6d3580a33ad571
Ram Elgov <ram.elgov@campus.technion.ac.il>
2021-10-18 00:19:08 +0300

    did part of question 3 (I need to check the graphs and to do the 3d task) Question 4a - passed given tests including empty string case. I want to add more test extreme cases.

--- hw/hw1/dry/dry_questions.py
+++ hw/hw1/dry/dry_questions.py
@@ -1,70 +1,105 @@
+import time
+
 """ Question 1 - solution """
-"""
-my_string = "xyzw"
-my_list = ['x','y','z','w']
-
-# string but not list functions
-
-print(str.capitalize(my_string))  # first method
-print(my_string.capitalize())     # second method
-
-print(str.replace(my_string, "xy","<(^__^)>"))  # first method
-print(my_string.replace("xy","<(^__^)>"))       # second method
-
-print(str.istitle(my_string))  # first method
-print(my_string.istitle())     # second method
-
-print("\n")
-
-# list but not string functions
-
-print(list.pop(my_list),)    # first method
-my_list = ['x','y','z','w']  # pop() is inplace
-print(my_list.pop())         # second method
-
-list.clear(my_list)            # first method
-print(my_list)
-my_list = ['x','y','z','w']    # clear() is inplace
-my_list.clear()                # second method\
-print(my_list)
-my_list = ['x','y','z','w']    # clear() is inplace
-
-
-list.insert(my_list, 3, "<(^__^)>")    # first method
-print(my_list)
-my_list = ['x','y','z','w']            # insert() is inplace
-my_list.insert(3, "<(^__^)>")          # second method
-print(my_list)
-
-"""
-
-
-def control_digit(israeli_id):
-    """
-    compute the check digit in an Israeli ID number,
-    given as a string of 8 digits.
-    :param israeli_id: Israeli ID number
-    :return: check digit
-    """
-    assert isinstance(israeli_id, str)
-    assert len(israeli_id) == 8
-
-    total = 0
-    for i in range(8):
-        idi = israeli_id[i]
-        val = int(israeli_id[i])  # converts chart to int
-        if i % 2 == 0:            # even index (0,2,4,6)
-            total += val
-        else:                     # odd index (1,3,5,7)
-            if val < 5:
-                total += 2 * val
-            else:
-                total += ((2 * val) % 10) + 1  # sum of digits in 2*val. 'tens' digits must be 1
-    total = total % 10                         # 'ones' (rightmost) digit
-    check_digit = (10 - total) % 10            # the complement modulo 10 of total
-
-    return str(check_digit)
+#
+# my_string = "xyzw"
+# my_list = ['x','y','z','w']
+#
+# # string but not list functions
+#
+# print(str.capitalize(my_string))  # first method
+# print(my_string.capitalize())     # second method
+#
+# print(str.replace(my_string, "xy","<(^__^)>"))  # first method
+# print(my_string.replace("xy","<(^__^)>"))       # second method
+#
+# print(str.istitle(my_string))  # first method
+# print(my_string.istitle())     # second method
+#
+# print("\n")
+#
+# # list but not string functions
+#
+# print(list.pop(my_list),)    # first method
+# my_list = ['x','y','z','w']  # pop() is inplace
+# print(my_list.pop())         # second method
+#
+# list.clear(my_list)            # first method
+# print(my_list)
+# my_list = ['x','y','z','w']    # clear() is inplace
+# my_list.clear()                # second method\
+# print(my_list)
+# my_list = ['x','y','z','w']    # clear() is inplace
+#
+#
+# list.insert(my_list, 3, "<(^__^)>")    # first method
+# print(my_list)
+# my_list = ['x','y','z','w']            # insert() is inplace
+# my_list.insert(3, "<(^__^)>")          # second method
+# print(my_list)
+#
 
 
-print(control_digit("20686751"))
+""" Question 2 - solution """
+# def control_digit(israeli_id):
+#     """
+#     compute the check digit in an Israeli ID number,
+#     given as a string of 8 digits.
+#     :param israeli_id: Israeli ID number
+#     :return: check digit
+#     """
+#     assert isinstance(israeli_id, str)
+#     assert len(israeli_id) == 8
+#
+#     total = 0
+#     for i in range(8):
+#         idi = israeli_id[i]
+#         val = int(israeli_id[i])  # converts chart to int
+#         if i % 2 == 0:            # even index (0,2,4,6)
+#             total += val
+#         else:                     # odd index (1,3,5,7)
+#             if val < 5:
+#                 total += 2 * val
+#             else:
+#                 total += ((2 * val) % 10) + 1  # sum of digits in 2*val. 'tens' digits must be 1
+#     total = total % 10                         # 'ones' (rightmost) digit
+#     check_digit = (10 - total) % 10            # the complement modulo 10 of total
+#
+#     return str(check_digit)
+#
+#
+# control_digit("20686751")
+
+""" Question 3 - solution """
+
+
+def zeros(num):  # 1st solution
+    m = num
+    cnt = 0
+    while m > 0:
+        if m % 10 == 0:
+            cnt = cnt + 1
+        m = m // 10
+    return cnt
+
+
+def zeros2(num):  # 2nd solution
+    cnt = 0
+    snum = str(num)  # num as a string
+    for digit in snum:
+        if digit == "0":
+            cnt = cnt + 1
+    return cnt
+
+
+def zeros3(num):  # 3rd solution
+    cnt = str.count(str(num), "0")
+    return cnt
+
+
+num = 2 ** 1400
+t0 = time.perf_counter()
+zeros3(num)
+t1 = time.perf_counter()
+print("Running time: ", t1 - t0, "sec")
 
--- hw/hw1/src/hw1_206867517.py
+++ hw/hw1/src/hw1_206867517.py
@@ -1,2 +1,84 @@
+# Skeleton file for HW1 - Spring 2021 - extended intro to CS
+
+# Add your implementation to this file
+
+# you may NOT change the signature of the existing functions.
+# you can add new functions if needed.
+
+# Change the name of the file to include your ID number (hw1_ID.py).
 
 
+# Question 4a
+def num_different_letters(text):
+    chars = "abcdefghijklmnopqrstuvwxyz"
+    chars_counter = [0 for i in range(26)]
+    if len(text) == 0:  # treating the case of an empty string
+        return 0
+
+    for i in range(len(text)):
+        if text[i] in chars:
+            chars_counter[chars.find(text[i])] = 1
+    return sum(chars_counter)
+
+# Question 4b
+def replace_char(text, old, new):
+    pass  # replace with your implementation
+
+
+# Question 4c
+def longest_word(text):
+    pass  # replace with your implementation
+
+
+# Question 4d
+def to_upper(text):
+    lower = "abcdefghijklmnopqrstuvwxyz"
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    pass  # replace with your implementation
+
+
+# Question 5
+def calc(expression):
+    pass  # replace with your implementation
+
+
+########
+# Tester
+########
+
+def test():
+    # testing Q4
+    if num_different_letters("aa bb cccc dd ee fghijklmnopqrstuvwxyz") != 26:
+        print("error in num_different_letters - 1")
+    if num_different_letters("aaa98765432100000000") != 1:
+        print("error in num_different_letters - 2")
+    if num_different_letters("") != 0:
+        print("error in num_different_letters - 3")
+
+    # if replace_char("abcdabcde", "a", "x") != "xbcdxbcde":
+    #     print("error in replace_char - 1")
+    # if replace_char("abcd123", "1", "x") != "abcdx23":
+    #     print("error in replace_char - 2")
+    #
+    # if longest_word("a bb ccc 4444 e") != 4:
+    #     print("error in longest_word - 1")
+    # if longest_word("a bb ccc 4444 eeeee fffff") != 5:
+    #     print("error in longest_word - 2")
+    #
+    # if to_upper("abc") != "ABC":
+    #     print("error in to_upper - 1")
+    # if to_upper("123") != "123":
+    #     print("error in to_upper - 2")
+    #
+    # # testing Q5
+    # if calc("'123321'*'2'") != "123321123321":
+    #     print("error in calc - 1")
+    # if calc("'Hi there '*'3'+'you2'") != "Hi there Hi there Hi there you2":
+    #     print("error in calc - 2")
+    # if calc("'hi+fi'*'2'*'2'") != "hi+fihi+fihi+fihi+fi":
+    #     print("error in calc - 3")
+    # if calc("'a'*'2'+'b'*'2'") != "aabaab":
+    #     print("error in calc - 4")
+
+
+test()
