b2ebe94bb8fe87dff0c5e22cd18fa4436a312935
Ram Elgov <ram.elgov@campus.technion.ac.il>
2021-10-26 13:16:50 +0300

    max_even_seq.py tests commented. As the task requires no other code than max_even_seq function itself.

--- hw/hw1/src/max_even_seq.py
+++ hw/hw1/src/max_even_seq.py
@@ -1,9 +1,9 @@
 def max_even_seq(n):
     """
-    finds the the maximal length of an even sequence of digits,
+    finds the the maximal length of an even subsequence of digits,
     that's included in n's decimal representation.
     :param n: a non-negative integer.
-    :return: max length of the sequence
+    :return: max length of the sub sequence
     """
 
     if n == 0:
@@ -28,36 +28,36 @@ def max_even_seq(n):
     return max_even
 
 
-def test():
-    if max_even_seq(23300247524689) != 4:
-        print("Error in test 1")
-    else:
-        print(max_even_seq(23300247524689))
-    if max_even_seq(0) != 1:
-        print("Error in test 2")
-    else:
-        print(max_even_seq(0))
-    if max_even_seq(13579751) != 0:
-        print("Error in test 3")
-    else:
-        print(max_even_seq(13579751))
-    if max_even_seq(2468645) != 6:
-        print("Error in test 4")
-    else:
-        print(max_even_seq(2468645))
-    if max_even_seq(12121212) != 1:
-        print("Error in test 5")
-    else:
-        print(max_even_seq(12121212))
-    if max_even_seq(808988) != 3:
-        print("Error in test 6")
-    else:
-        print(max_even_seq(808988))
-    if max_even_seq(2) != 1:
-        print("Error in test 7")
-    else:
-        print(max_even_seq(2))
-    if max_even_seq(7) != 0:
-        print("Error in test 8")
-    else:
-        print(max_even_seq(7))
+# def test():
+#     if max_even_seq(23300247524689) != 4:
+#         print("Error in test 1")
+#     else:
+#         print(max_even_seq(23300247524689))
+#     if max_even_seq(0) != 1:
+#         print("Error in test 2")
+#     else:
+#         print(max_even_seq(0))
+#     if max_even_seq(13579751) != 0:
+#         print("Error in test 3")
+#     else:
+#         print(max_even_seq(13579751))
+#     if max_even_seq(2468645) != 6:
+#         print("Error in test 4")
+#     else:
+#         print(max_even_seq(2468645))
+#     if max_even_seq(12121212) != 1:
+#         print("Error in test 5")
+#     else:
+#         print(max_even_seq(12121212))
+#     if max_even_seq(808988) != 3:
+#         print("Error in test 6")
+#     else:
+#         print(max_even_seq(808988))
+#     if max_even_seq(2) != 1:
+#         print("Error in test 7")
+#     else:
+#         print(max_even_seq(2))
+#     if max_even_seq(7) != 0:
+#         print("Error in test 8")
+#     else:
+#         print(max_even_seq(7))
