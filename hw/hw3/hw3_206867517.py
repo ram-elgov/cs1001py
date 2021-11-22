fc8b30fac192fdc536167a3f4a99afe65d052499
Ram Elgov <ram.elgov@campus.technion.ac.il>
2021-11-22 20:28:57 +0200

    Until Q5b (inclusive) tests passed.

--- hw/hw3/hw3_206867517.py
+++ hw/hw3/hw3_206867517.py
@@ -39,43 +39,131 @@ def is_greater_equal(bin1, bin2):
         fraction_plus_one2 += 2 ** (8 - i) if bin2[i] == "1" else 0
     sign1 = 1 if bin1[0] == "1" else 0
     sign2 = 1 if bin2[0] == "1" else 0
-    return ((-1) ** sign1) * (2 ** (exp1 - 127)) * fraction_plus_one1 >=\
+    return ((-1) ** sign1) * (2 ** (exp1 - 127)) * fraction_plus_one1 >= \
            ((-1) ** sign2) * (2 ** (exp2 - 127)) * fraction_plus_one2
 
 
 # Q3 - A
 def approx_root(x, e):
-    pass  # replace this with your code
+    approx = 0  # the current approximation
+    lst = [1]  # list of a_1,a_2,...,a_n
+    product = 1  # a_1*a_2*...*a_k
+    while x >= (approx + e) * (approx + e):  # check error condition
+        n = lst[len(lst) - 1]  # set n to be a_n (invariant - a_n-1 <= a_n)
+        while (approx + 1 / (product * n)) * (approx + 1 / (product * n)) > x:  # check the approximation from below
+            # condition
+            n += 1
+        approx += 1 / (product * n)  # calculate new approximation
+        lst.append(n)  # n -> a_n
+        product = product * n  # update product
+    return lst[1:], approx  # remove the first redundant "1" before return
 
 
 # Q3 - B
 def approx_e(N):
-    pass  # replace this with your code
+    sum_of_N_games = 0
+    for i in range(N):
+        sum_of_N_games += approx_game()
+    return sum_of_N_games / N
+
+
+def approx_game():
+    sum_of_r = 0
+    n = 0
+    while sum_of_r <= 1:
+        n += 1
+        sum_of_r += random.random()
+    return n
 
 
 # Q4 - A
 def find(lst, s):
-    pass  # replace this with your code
+    n = len(lst)
+    left = 0
+    right = n - 1
+    while left <= right:
+        mid = (left + right) // 2  # middle rounded down
+        if s == lst[mid]:  # item found
+            return mid
+        if mid > 0 and lst[mid - 1] == s:  # item found
+            return mid - 1
+        if mid < n - 1 and lst[mid + 1] == s:  # item found
+            return mid + 1
+        if s < lst[mid]:  # item cannot be in top half (and not in midi's left adjacent)
+            right = mid - 2
+        else:  # item cannot be in bottom half  (and not in midi's right adjacent)
+            left = mid + 2
+    return None
 
 
 # Q4 - B
 def sort_from_almost(lst):
-    pass  # replace this with your code
+    """ sorts an almost sorted array inplace. time: O(n), memory: O(1) """
+    if len(lst) != 0:
+        for i in range(len(lst) - 1):  # O(n) time
+            if lst[i] > lst[i + 1]:
+                swap(lst, i, i + 1)  # O(1) time O(1) memory
+    return lst
+
+
+def swap(lst, i1, i2):
+    """ swaps items in given indexes inplace. time: O(1), memory: O(1) """
+    temp = lst[i2]
+    lst[i2] = lst[i1]
+    lst[i1] = temp
 
 
 # Q4 - C
 def find_local_min(lst):
-    pass  # replace this with your code
+    """ input: non empty list return: a local minima index """
+    left = 0
+    right = len(lst) - 1
+    while left <= right:
+        mid = (right + left) // 2
+        if ((mid == 0 or lst[mid - 1] >= lst[mid]) and
+                (mid == len(lst) - 1 or lst[mid] <= lst[mid + 1])):
+            return mid
+        elif mid < len(lst) - 1 and lst[mid + 1] < lst[mid]:
+            left = mid + 1
+        elif mid > 0 and lst[mid - 1] < lst[mid]:
+            right = mid - 1
 
 
 # Q5 - A
 def string_to_int(s):
-    pass  # replace this with your code
+    k = len(s)
+    d = {
+        "a": 0,
+        "b": 1,
+        "c": 2,
+        "d": 3,
+        "e": 4,
+    }
+    code = 0
+    for i in range(k):
+        code += d[s[i]] * (5 ** (k - i - 1))
+    return code
 
 
 # Q5 - B
 def int_to_string(k, n):
-    pass  # replace this with your code
+    d = {
+        "e": 4,
+        "d": 3,
+        "c": 2,
+        "b": 1,
+        "a": 0,
+    }
+    s = ""
+    for i in range(k):
+        for word in d:
+            exp = d[word] * (5 ** (k - i - 1))
+            if exp <= n:
+                s += word
+                if word != "a":
+                    n -= exp
+                break
+    return s
 
 
 # Q5 - C
@@ -96,8 +184,8 @@ def test():
     # if bin_to_fraction('01101') != 0.40625 or bin_to_fraction('1010000') != 0.625:
     #     print('error in bin_to_fraction')
     # Q2 - D
-    if bin_to_float('00111110001000000000000000000000') != 0.15625:
-        print("error in bin_to_float")
+    # if bin_to_float('00111110001000000000000000000000') != 0.15625:
+    #     print("error in bin_to_float")
     # # Q2 - E
     # if is_greater_equal('00111110001000000000000000000000', '00111111001000000000000000000000') == True or \
     #         is_greater_equal('00111110001000000000000000000000', '00111110001000000000000000000000') == False:
@@ -113,30 +201,40 @@ def test():
     # if abs(approx_e(1000000) - math.e) > 0.01:
     #     print("MOST LIKELY there's an error in approx_e (this is a probabilistic test)")
     #
-    # # Q4 - A
+    # Q4 - A
     # almost_sorted_lst = [2, 1, 3, 5, 4, 7, 6, 8, 9]
     # if find(almost_sorted_lst, 5) != 3:
     #     print("error in find")
-    # if find(almost_sorted_lst, 50) != None:
+    # if find(almost_sorted_lst, 50) is not None:
     #     print("error in find")
     # # Q4 - B
-    # if sort_from_almost(almost_sorted_lst) != sorted(almost_sorted_lst):
+    # if sort_from_almost(almost_sorted_lst) != sorted(almost_sorted_lst) or \
+    #         not (sort_from_almost(almost_sorted_lst) is almost_sorted_lst):
     #     print("error in sort_from_almost")
-    # # Q4 - C
+
+    # Q4 - C
     # lst = [5, 6, 7, 5, 1, 1, 99, 100]
     # pos = find_local_min(lst)
     # if pos not in (0, 4, 5):
     #     print("error in find_local_min")
-    #
-    # # Q5
-    # lst_num = [random.choice(range(5 ** 4)) for i in range(15)]
-    # for i in lst_num:
-    #     s = int_to_string(4, i)
-    #     if s is None or len(s) != 4:
-    #         print("error in int_to_string")
-    #     if string_to_int(s) != i:
-    #         print("error in int_to_string and/or in string_to_int")
-    #
+
+    # Q5
+    lst_num = [random.choice(range(5 ** 4)) for _ in range(15)]
+    for i in lst_num:
+        s = int_to_string(4, i)
+        if s is None or len(s) != 4:
+            print("error in int_to_string")
+        if string_to_int(s) != i:
+            print("error in int_to_string and/or in string_to_int")
+    for i in range(5 ** 3):
+        if string_to_int(int_to_string(3, i)) != i:
+            print("Problem with ", i)
+    alphabet = ["a", "b", "c", "d", "e"]
+    lst = [x + y + z for x in alphabet for y in alphabet for z in alphabet]
+    for item in lst:
+        if int_to_string(3, string_to_int(item)) != item:
+            print("Problem with ", item)
+
     # lst1 = ['aede', 'adae', 'dded', 'deea', 'cccc', 'aacc', 'edea', 'becb', 'daea', 'ccea']
     # if sort_strings1(lst1, 4) \
     #         != ['aacc', 'adae', 'aede', 'becb', 'cccc', 'ccea', 'daea', 'dded', 'deea', 'edea']:
--- hw/hw3/hw3_206867517_tests.py
+++ hw/hw3/hw3_206867517_tests.py
@@ -1,3 +1,4 @@
+import math
 import unittest
 import hw3_206867517 as hw3
 
@@ -12,14 +13,15 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(0.625, hw3.bin_to_fraction('1010000'))
 
         # Q2 - D
-    def test_bin_to_float(self):
-            self.assertEqual(0.15625, hw3.bin_to_float('00111110001000000000000000000000'))
-            self.assertEqual(2, hw3.bin_to_float('01000000000000000000000000000000'))
-            self.assertEqual(-2, hw3.bin_to_float('11000000000000000000000000000000'))
-            self.assertEqual(-36893488147419103232, hw3.bin_to_float('11100000000000000000000000000000'))
-            self.assertEqual(-118842243771396506390315925504, hw3.bin_to_float('111011111100000000000000000000000'))
 
-        # # Q2 - E
+    def test_bin_to_float(self):
+        self.assertEqual(0.15625, hw3.bin_to_float('00111110001000000000000000000000'))
+        self.assertEqual(2, hw3.bin_to_float('01000000000000000000000000000000'))
+        self.assertEqual(-2, hw3.bin_to_float('11000000000000000000000000000000'))
+        self.assertEqual(-36893488147419103232, hw3.bin_to_float('11100000000000000000000000000000'))
+        self.assertEqual(-118842243771396506390315925504, hw3.bin_to_float('111011111100000000000000000000000'))
+
+    # Q2 - E
     def test_is_greater_equal(self):
         self.assertEqual(False,
                          hw3.is_greater_equal('00111110001000000000000000000000', '00111111001000000000000000000000'))
@@ -36,49 +38,125 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(True,
                          hw3.is_greater_equal('01000000110000000000000000000000', '11000000011000000000000000000000'))
 
-        # # Q3 - A
-        # if approx_root(2, 0.1) != ([1, 3], 1 + 1 / 3):
-        #     print("error in approx_root (1)")
-        # if approx_root(2, 0.02) != ([1, 3, 5], 1 + 1 / 3 + 1 / 15):
-        #     print("error in approx_root (2)")
-        # if approx_root(2, 0.001) != ([1, 3, 5, 5], 1 + 1 / 3 + 1 / 15 + 1 / 75):
-        #     print("error in approx_root (3)")
-        # # Q3 - B
-        # if abs(approx_e(1000000) - math.e) > 0.01:
-        #     print("MOST LIKELY there's an error in approx_e (this is a probabilistic test)")
-        #
-        # # Q4 - A
-        # almost_sorted_lst = [2, 1, 3, 5, 4, 7, 6, 8, 9]
-        # if find(almost_sorted_lst, 5) != 3:
-        #     print("error in find")
-        # if find(almost_sorted_lst, 50) != None:
-        #     print("error in find")
-        # # Q4 - B
-        # if sort_from_almost(almost_sorted_lst) != sorted(almost_sorted_lst):
-        #     print("error in sort_from_almost")
-        # # Q4 - C
-        # lst = [5, 6, 7, 5, 1, 1, 99, 100]
-        # pos = find_local_min(lst)
-        # if pos not in (0, 4, 5):
-        #     print("error in find_local_min")
-        #
-        # # Q5
-        # lst_num = [random.choice(range(5 ** 4)) for i in range(15)]
-        # for i in lst_num:
-        #     s = int_to_string(4, i)
-        #     if s is None or len(s) != 4:
-        #         print("error in int_to_string")
-        #     if string_to_int(s) != i:
-        #         print("error in int_to_string and/or in string_to_int")
-        #
-        # lst1 = ['aede', 'adae', 'dded', 'deea', 'cccc', 'aacc', 'edea', 'becb', 'daea', 'ccea']
-        # if sort_strings1(lst1, 4) \
-        #         != ['aacc', 'adae', 'aede', 'becb', 'cccc', 'ccea', 'daea', 'dded', 'deea', 'edea']:
-        #     print("error in sort_strings1")
-        #
-        # if sort_strings2(lst1, 4) \
-        #         != ['aacc', 'adae', 'aede', 'becb', 'cccc', 'ccea', 'daea', 'dded', 'deea', 'edea']:
-        #     print("error in sort_strings2")
+        # Q3 - A
+
+    def test_approx_root(self):
+        self.assertEqual(([1, 3, 5], 1 + 1 / 3 + 1 / 15), hw3.approx_root(2, 0.02))
+        self.assertEqual(([1, 3, 5, 5], 1 + 1 / 3 + 1 / 15 + 1 / 75), hw3.approx_root(2, 0.001))
+        self.assertEqual(([1, 3], 1 + 1 / 3), hw3.approx_root(2, 0.1))
+        self.assertEqual(([1, 1, 5], 2.2), hw3.approx_root(5, 0.1))
+        self.assertEqual(([1, 1, 1], 3), hw3.approx_root(9, 0.1))
+        self.assertEqual(([1, 1, 3, 3], 2.4444444444444446), hw3.approx_root(6, 0.1))
+        self.assertEqual(([1, 1, 2, 2], 2.75), hw3.approx_root(8, 0.1))
+        self.assertEqual(([1, 1, 2, 2, 4, 4], 2.828125), hw3.approx_root(8, 0.01))
+        self.assertEqual(([1, 3, 5, 5, 16, 18, 78], 1.4142135565052232), hw3.approx_root(2, 0.00000001))
+        self.assertEqual(([1, 1, 5, 6, 13, 16, 16, 38, 48, 58], 2.236067977498874),
+                         hw3.approx_root(5, 0.00000000001))
+
+    # # Q3 - B
+    def test_approx_e(self):
+        self.assertEqual(True, abs(math.e - hw3.approx_e(1000000)) < 0.1)
+        self.assertEqual(True, abs(math.e - hw3.approx_e(100)) < 0.3)
+        self.assertEqual(True, abs(math.e - hw3.approx_e(1000)) < 0.2)
+        self.assertEqual(True, abs(math.e - hw3.approx_e(10000)) < 0.1)
+        self.assertEqual(False, abs(math.e - hw3.approx_e(1)) < 0.2)
+
+    # Q4 - A
+    def test_find(self):
+        self.assertEqual(3, hw3.find([2, 1, 3, 5, 4, 7, 6, 8, 9], 5))
+        self.assertEqual(0, hw3.find([2, 1, 3, 5, 4, 7, 6, 8, 9], 2))
+        self.assertEqual(1, hw3.find([2, 1, 3, 5, 4, 7, 6, 8, 9], 1))
+        self.assertEqual(2, hw3.find([2, 1, 3, 5, 4, 7, 6, 8, 9], 3))
+        self.assertEqual(4, hw3.find([2, 1, 3, 5, 4, 7, 6, 8, 9], 4))
+        self.assertEqual(5, hw3.find([2, 1, 3, 5, 4, 7, 6, 8, 9], 7))
+        self.assertEqual(6, hw3.find([2, 1, 3, 5, 4, 7, 6, 8, 9], 6))
+        self.assertEqual(7, hw3.find([2, 1, 3, 5, 4, 7, 6, 8, 9], 8))
+        self.assertEqual(8, hw3.find([2, 1, 3, 5, 4, 7, 6, 8, 9], 9))
+        self.assertEqual(None, hw3.find([2, 1, 3, 5, 4, 7, 6, 8, 9], 0))
+        self.assertEqual(None, hw3.find([2, 1, 3, 5, 4, 7, 6, 8, 9], 10))
+        self.assertEqual(0, hw3.find([2, 1, 3], 2))
+        self.assertEqual(3, hw3.find([2, 1, 3, 5], 5))
+        self.assertEqual(1, hw3.find([2, 1], 1))
+        self.assertEqual(None, hw3.find([], 11))
+
+    # Q4 - B
+    def test_sort_from_almost(self):
+        lst = [2, 1, 3, 5, 4, 7, 6, 8, 9]
+
+        self.assertEqual([1, 2, 3, 4, 5, 6, 7, 8, 9], hw3.sort_from_almost(lst))
+        self.assertIs(lst, hw3.sort_from_almost(lst))
+
+        lst = [2, 1, 4, 3, 6, 5, 8, 7, 9]
+
+        self.assertEqual([1, 2, 3, 4, 5, 6, 7, 8, 9], hw3.sort_from_almost(lst))
+        self.assertIs(lst, hw3.sort_from_almost(lst))
+
+        lst = [1, 1, 1, 1, 1, 2, 1]
+
+        self.assertEqual([1, 1, 1, 1, 1, 1, 2], hw3.sort_from_almost(lst))
+        self.assertIs(lst, hw3.sort_from_almost(lst))
+
+        lst = [2, 1]
+
+        self.assertEqual([1, 2], hw3.sort_from_almost(lst))
+        self.assertIs(lst, hw3.sort_from_almost(lst))
+
+        lst = []
+        self.assertEqual([], hw3.sort_from_almost(lst))
+        self.assertIs(lst, hw3.sort_from_almost(lst))
+
+    # Q4 - C
+    def test_find_local_min(self):
+        self.assertEqual(4, hw3.find_local_min([5, 4, 3, 2, 1, 2, 3, 4, 5]))
+        self.assertEqual(3, hw3.find_local_min([5, 4, 3, 1, 2, 2.5, 3, 4, 5]))
+        self.assertEqual(2, hw3.find_local_min([5, 4, 1, 2, 2.5, 2.75, 3, 4, 5]))
+        self.assertEqual(1, hw3.find_local_min([5, 1, 2.1, 2.2, 2.3, 2.4, 3, 4, 5]))
+        self.assertEqual(0, hw3.find_local_min([1, 2, 3, 4, 5, 6, 7, 8]))
+        self.assertEqual(6, hw3.find_local_min([7, 6, 5, 4, 3, 2, 1]))
+
+    # Q5
+    def test_injective_func(self):
+        str1 = 'aaaaaaaaa'
+        self.assertEqual(str1, hw3.int_to_string(len(str1), hw3.string_to_int(str1)))
+        str1 = "abaaaaaaa"
+        self.assertEqual(str1, hw3.int_to_string(len(str1), hw3.string_to_int(str1)))
+        str1 = "aacaaaaaa"
+        self.assertEqual(str1, hw3.int_to_string(len(str1), hw3.string_to_int(str1)))
+        str1 = "aaadaaaaa"
+        self.assertEqual(str1, hw3.int_to_string(len(str1), hw3.string_to_int(str1)))
+        str1 = "aaaaaeaaa"
+        self.assertEqual(str1, hw3.int_to_string(len(str1), hw3.string_to_int(str1)))
+        str1 = "aaaaaeeaa"
+        self.assertEqual(str1, hw3.int_to_string(len(str1), hw3.string_to_int(str1)))
+        str1 = "aaaaaeaea"
+        self.assertEqual(str1, hw3.int_to_string(len(str1), hw3.string_to_int(str1)))
+        str1 = "eaaaaeaae"
+        self.assertEqual(str1, hw3.int_to_string(len(str1), hw3.string_to_int(str1)))
+        str1 = "eeeeeeeee"
+        self.assertEqual(str1, hw3.int_to_string(len(str1), hw3.string_to_int(str1)))
+        str1 = "ddeeccaab"
+        self.assertEqual(str1, hw3.int_to_string(len(str1), hw3.string_to_int(str1)))
+        str1 = "abcdeabcd"
+        self.assertEqual(str1, hw3.int_to_string(len(str1), hw3.string_to_int(str1)))
+        str1 = "aede"
+        self.assertEqual(str1, hw3.int_to_string(len(str1), hw3.string_to_int(str1)))
+
+    # lst_num = [random.choice(range(5 ** 4)) for i in range(15)]
+    # for i in lst_num:
+    #     s = int_to_string(4, i)
+    #     if s is None or len(s) != 4:
+    #         print("error in int_to_string")
+    #     if string_to_int(s) != i:
+    #         print("error in int_to_string and/or in string_to_int")
+    #
+    # lst1 = ['aede', 'adae', 'dded', 'deea', 'cccc', 'aacc', 'edea', 'becb', 'daea', 'ccea']
+    # if sort_strings1(lst1, 4) \
+    #         != ['aacc', 'adae', 'aede', 'becb', 'cccc', 'ccea', 'daea', 'dded', 'deea', 'edea']:
+    #     print("error in sort_strings1")
+    #
+    # if sort_strings2(lst1, 4) \
+    #         != ['aacc', 'adae', 'aede', 'becb', 'cccc', 'ccea', 'daea', 'dded', 'deea', 'edea']:
+    #     print("error in sort_strings2")
 
 
 if __name__ == '__main__':
